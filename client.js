904323c44474fa3cc00ad2707d279fdb4c196537
Marta Jopp <martapeterson@gmail.com>
2018-04-24 21:30:17 -0500

    handle invalid keys

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
 console.log('sourced');
 
 window.addEventListener('keydown', function (event){
-    console.log(event)
+
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    console.log(audio)
+
+    if (!audio) return //handles invalid keys that do not have audio attached stops function
+    audio.play();
 })
