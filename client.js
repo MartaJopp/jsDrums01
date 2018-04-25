84a58dab4efb125b2eb7ba52add7b26ad2b83585
Marta Jopp <martapeterson@gmail.com>
2018-04-24 21:35:16 -0500

    handles multiple keys

--- client.js
+++ client.js
@@ -3,8 +3,11 @@ console.log('sourced');
 window.addEventListener('keydown', function (event){
 
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
     console.log(audio)
 
     if (!audio) return //handles invalid keys that do not have audio attached stops function
+    audio.currentTime = 0 //rewinds to start if hit over and over again
     audio.play()
+    console.log(key)
 })
